c3d2e3bd154430a257b9c74d2c6723c55b0b53e6
Niih PC <lucasmassarico1@gmail.com>
2023-06-26 08:54:13 -0300

    Interface

---
+++ modules/Interface/campeao.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var championIcons = document.querySelectorAll('.icon-container img');
+    var championContainer = document.querySelector('.y-champion-container-img');
+
+    championIcons.forEach(function(icon) {
+        icon.addEventListener('click', function() {
+            var championName = icon.alt;
+            var championImage = icon.src;
+
+            var championContent = '<img src="' + championImage + '" alt="' + championName + '">';
+            championContainer.innerHTML = championContent;
+        });
+    });
+});
+
+const championImages = document.querySelectorAll('.icon-container img');
+const yChampionContainer = document.querySelector('.y-champion-container');
+
+championImages.forEach(function(image) {
+    image.addEventListener('click', function() {
+        yChampionContainer.style.display = 'flex';
+    });
+});
---
+++ modules/Interface/script.js
@@ -0,0 +1,17 @@
+const searchInput = document.getElementById('search-input');
+const champions = document.querySelectorAll('.icon-container div');
+
+searchInput.addEventListener('input', function() {
+    const searchText = searchInput.value.toLowerCase();
+
+    champions.forEach(function(champion) {
+        const championName = champion.querySelector('span').textContent.toLowerCase();
+        const championMatchesSearch = championName.includes(searchText);
+
+        if (championMatchesSearch) {
+            champion.style.display = 'block';
+        } else {
+            champion.style.display = 'none';
+        }
+    });
+});
